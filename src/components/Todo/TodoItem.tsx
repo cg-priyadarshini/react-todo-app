f5859bf13f7bc26a0c89394896973b58e2a82ffd
cg-priyadarshini <142870691+cg-priyadarshini@users.noreply.github.com>
2024-03-12 16:29:30 +0530

    Add files via upload

--- src/components/Todo/TodoItem.tsx
+++ src/components/Todo/TodoItem.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import { TodoItemProps } from "../../types/todo";
 import { Button, Input } from "../common";
 import {Tooltip} from 'react-tooltip';
